10dd87834eeb4207b746e79bcbb74a72f1072d6d
Viktor Koleneko <vkoleneko@gmail.com>
2021-05-23 12:32:15 +0300

    Imported to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/style.css';
+import Logo from './img/Logo.svg';
+import Search_Button from './img/Search_Button.svg';
+import './style/fonts/font-inter/stylesheet.css';
+import Avatar from './img/profile_img.jpg';
+import Following from './img/Following.svg';
+import Followers from './img/Followers.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Header">
+        <img className="Logo" src={Logo} alt="img" />
+        <div className="Search">
+          <a className="Search_Button" href="#">
+            <img src={Search_Button} alt="img" />
+          </a>
+          <input className="Input" type="text" placeholder="Enter GitHub username" />
+        </div>
+      </div>
+      <div className="Main_page">
+        <div className="Wrapper">
+          <div className="user_data">
+            <img className="Avatar" src={Avatar} alt="Avatar" />
+            <div className="User_data_wrapper">
+              <p className="Name">Dan Abramov</p>
+              <a className="Link" href="">
+                gaearon
+              </a>
+              <div className="Metrics_wrapper">
+                <div className="Metrics">
+                  <img src={Followers} alt="" />
+                  <p className="Metrics_text">65.8k followers</p>
+                </div>
+                <div className="Metrics">
+                  <img src={Following} alt="" />
+                  <p className="Metrics_text">171 following</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="Repository">
+            <h1 className="Repo_main">Repositories (249)</h1>
+            <div className="Repositories_blocks">
+              <a className="Repo_link" href="#">
+                react-hot-loader
+              </a>
+              <p className="Repo_description">
+                Tweak React components in real time. (Deprecated: use Fast Refresh instead.)
+              </p>
+            </div>
+            <div className="Repositories_blocks">
+              <a className="Repo_link" href="#">
+                react-hot-loader
+              </a>
+              <p className="Repo_description">
+                Tweak React components in real time. (Deprecated: use Fast Refresh instead.)
+              </p>
+            </div>
+            <div className="Repositories_blocks">
+              <a className="Repo_link" href="#">
+                react-hot-loader
+              </a>
+              <p className="Repo_description">
+                Tweak React components in real time. (Deprecated: use Fast Refresh instead.)
+              </p>
+            </div>
+            <div className="Repositories_blocks">
+              <a className="Repo_link" href="#">
+                react-hot-loader
+              </a>
+              <p className="Repo_description">
+                Tweak React components in real time. (Deprecated: use Fast Refresh instead.)
+              </p>
+            </div>
+            <div className="Pagination">Pagination template</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
